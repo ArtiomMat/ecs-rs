1bd4f5f4b572c9bb25940785fa3228213dae4946
Your Name <you@example.com>
2026-08-19 01:22:23 +0300

    Remove default implementation for trait QueryParam

--- src/ecs/query/query_param.rs
+++ src/ecs/query/query_param.rs
@@ -5,13 +5,9 @@ use crate::ecs::{EntityId, World};
 pub trait QueryParam<'w> {
     type Output;
 
-    fn guaranteed_matches_len(world: &'w World) -> usize {
-        world.entity_validity_set.len()
-    }
+    fn guaranteed_matches_len(world: &'w World) -> usize;
 
-    fn guaranteed_matches_iter(world: &'w World) -> Box<dyn Iterator<Item = &'w EntityId> + 'w> {
-        Box::new(world.entity_validity_set.iter())
-    }
+    fn guaranteed_matches_iter(world: &'w World) -> Box<dyn Iterator<Item = &'w EntityId> + 'w>;
 
     fn matches(world: &'w World, entity_id: EntityId) -> bool;
     fn fetch(world: &'w World, entity_id: EntityId) -> Self::Output;
